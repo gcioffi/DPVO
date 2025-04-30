4b5bc39372a8eec00c4525e9454165d497341332
Giovanni <cioffi.giovanni.01@gmail.com>
2025-04-30 10:40:27 +0200

    modify traj saving

--- demo_vslam.py
+++ demo_vslam.py
@@ -61,10 +61,9 @@ def run(cfg, network, imagedir, calib, fisheye, stride=1, viz=False, show_img=Fa
 
     reader.join()
 
-    odom_traj, _ = slam.return_trajectory()
     slam_traj, timestamps = slam.terminate()
 
-    return odom_traj, slam_traj, timestamps
+    return slam_traj, timestamps
 
 
 if __name__ == '__main__':
@@ -100,31 +99,22 @@ if __name__ == '__main__':
     
     print("\nRunning VO...")
     calib_fn = os.path.join("calib", args.calib)
-    odomtraj_est, slamtraj_est, _ = run(cfg, args.network, imagedir, calib_fn, args.fisheye, args.stride, args.viz, args.show_img)
+    slamtraj_est, _ = run(cfg, args.network, imagedir, calib_fn, args.fisheye, args.stride, args.viz, args.show_img)
 
     if args.out_traj_prefix is not None:
         # remove
         images_list = sorted(glob.glob(os.path.join(imagedir, "*.png")))[::args.stride]
         tstamps = np.asarray([float(x.split('/')[-1][:-4]) for x in images_list])
 
-        assert odomtraj_est.shape[0] == tstamps.shape[0], "Odom Trajectory length does not match number of images"
         assert slamtraj_est.shape[0] == tstamps.shape[0], "SLAM Trajectory length does not match number of images"
-
-        odomtraj_out = np.zeros((tstamps.shape[0], 8))
-        odomtraj_out[:, 0] = tstamps * 1e-9
-        odomtraj_out[:, 1:] = odomtraj_est
 
         slamtraj_out = np.zeros((tstamps.shape[0], 8))
         slamtraj_out[:, 0] = tstamps * 1e-9
         slamtraj_out[:, 1:] = slamtraj_est
 
-        out_odomtrajfn = args.out_traj_prefix + '_stamped_odom_traj_estimate.txt'
-        np.savetxt(out_odomtrajfn, odomtraj_out, fmt='%.6f', header='ts x y z qx qy qz qw')
-        print("Saved trajectory to {}".format(out_odomtrajfn))
-
-        out_slamtrajfn = args.out_traj_prefix + '_stamped_slam_traj_estimate.txt'
+        out_slamtrajfn = args.out_traj_prefix + '_stamped_traj_estimate.txt'
         if cfg.LOOP_CLOSURE:
-            out_slamtrajfn = args.out_traj_prefix + '_stamped_slamlc_traj_estimate.txt'
+            out_slamtrajfn = args.out_traj_prefix + '_stamped_traj_estimate_with_loopclosure.txt'
             
         np.savetxt(out_slamtrajfn, slamtraj_out, fmt='%.6f', header='ts x y z qx qy qz qw')
         print("Saved trajectory to {}".format(out_slamtrajfn))
