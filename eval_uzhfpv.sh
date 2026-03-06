f94f614b34d5259b2cf70a9b2cbf6a6f7905648a
Giovanni <cioffi.giovanni.01@gmail.com>
2026-03-06 16:13:16 +0100

    script to launch uzhfpv exp

--- eval_uzhfpv.sh
+++ eval_uzhfpv.sh
@@ -1,29 +1,77 @@
 # Indoor forward
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3 --opts LOOP_CLOSURE True
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_1  --opts LOOP_CLOSURE True 
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_2  --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_3_3  --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_1  --opts LOOP_CLOSURE True 
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_2  --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_6_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_6_3  --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_forward_7_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_forward_7_3 --opts LOOP_CLOSURE True
 
 # Indoor 45
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2 --opts LOOP_CLOSURE True
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_2_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_2_3 --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_1  --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_2  --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_9_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_9_3  --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=indoor_45_14_snapdragon_with_gt/img/left --calib=uzhfpv_indoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/indoor_45_14_3 --opts LOOP_CLOSURE True
 
 # Outdoor forward
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1 --opts LOOP_CLOSURE True
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3 --opts LOOP_CLOSURE True
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5 --opts LOOP_CLOSURE True
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_10_snapdragon/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_10
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_10_snapdragon/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_10 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_1_3 --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_3_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_3_3 --opts LOOP_CLOSURE True
+
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_forward_5_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_forward.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_forward_5_3 --opts LOOP_CLOSURE True
 
 # Outdoor 45
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_2
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_3
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_2 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=outdoor_45_1_snapdragon_with_gt/img/left --calib=uzhfpv_outdoor_45.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/outdoor_45_1_3 --opts LOOP_CLOSURE True
 
 # Race 1
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=race_1/img/no_init_static_part --calib=uzhfpv_race.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/race_1
-python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=race_1/img/no_init_static_part --calib=uzhfpv_race.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/race_1 --opts LOOP_CLOSURE True
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=race_1/img/ --calib=uzhfpv_race.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/race_1
+python demo_vslam.py --datasetdir=/datasets/UZH_FPV --scene=race_1/img/ --calib=uzhfpv_race.txt --fisheye --out_traj_prefix=/home/giovanni/DPVO_docker/DPVO/race_1 --opts LOOP_CLOSURE True
